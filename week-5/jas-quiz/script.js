0545399c716b9985f76d74b5303195fea81b8c15
Nurhan-Garang <garangnurhan@gmail.com>
2025-07-28 20:31:21 +0300

    Intial commit

---
+++ week-5/jas-quiz/script.js
@@ -0,0 +1,47 @@
+let name="john";
+console.log("hello " + name +"!");
+let age=20;
+ console.log(typeof age);
+ let greeting="hello";
+ console.log(typeof greeting);
+ let num=123;
+ console.log(typeof num);
+ let israining=true;
+ console.log(typeof israining);
+ let dream= "empty"
+ console.log(typeof dream)
+ let num1=15
+ let num2=14
+ console.log(num1%num2)
+ let cookies=10
+ let milks=5
+ console.log(cookies>milks)
+ let books=5
+ let pens="5"
+ console.log(books==pens)
+ console.log(books===pens)
+ let students=50
+ let newstudent=5
+ console.log(students+newstudent)
+ let buses=10
+ console.log(buses-1)
+ let vote=18
+ let person=17
+ if(vote>=18){
+    console.log("can vote")
+ }else{
+    console.log("can not")
+ }
+ let studenta=60
+ if(studenta>=80){
+    console.log("good job")
+ }else {
+console.log("nice try")
+ }
+let userName="admin"
+let hasPassword=false
+if(userName="admin"&&hasPassword){
+    console.log("login successful")
+} else {
+    console.log("Access denied" )
+}
---
+++ week-5/javascript/script.js
@@ -0,0 +1,106 @@
+
+// let day=1;
+// let dayName;
+// switch(day){
+//     case 1:
+//        console.log("dayName:Monday");
+//      break;
+//      case 2:
+//         console.log("dayName:Tuesday")
+//         break;
+//         case 3:
+//             console.log("dayName:wednesday")
+//             break;
+//             case 4:
+//                 console.log("dayName:Thursday")
+//                 break;
+//                 case 5:
+//                     console.log("dayName:friday")
+//                     break;
+
+// }
+// let grade=a;
+// let studentGrade;
+// switch(grade){
+//   case a:
+//     console.log("studentGrade:excellent");
+//     break;
+//     case b:
+//         console.log("studentGrade:good");
+//         break;
+//         case c:
+//             console.log("studentGrade:average");
+//             break;
+//             case d:
+//                 console.log("studentGrade:below");
+//                 break;
+//                 case f:
+//                     console.log("studentGrade:fail");
+//                     break;
+//                     default:
+//                         console.log("invalid grade");
+// }
+// for(let i=0;i<=5;i++){
+//     console.log("the number is " +i)
+// }
+// for(let word=0;word<=3;word++){
+//     console.log("Hello "+word)
+// };
+// for(let i=2;i<=10;i+=2){
+//     console.log("even",i);
+// }
+for(let i=10;i>=1;i--){
+    console.log("Number",i);
+}
+for(let i=1;i<=5;i++){
+    console.log("square"*i)
+}
+for(let i=1;i<=20;i){
+ if(i%3===0){
+    console.log(i)
+ }
+}
+let fruit="apple"
+switch(fruit){
+    case"apple":
+    console.log("Apples are sweet!");
+    break;
+    case"Banana":
+    console.log("Bananas are yellow!");
+    break;
+    case"orange":
+    console.log("Oranges are juicy!");
+    break;
+    default:
+        console.log("I dont know that fruit.");
+}
+let i=0;
+while(i<0){
+    console.log("counting:"+i);
+    i++;
+}
+let pasword="";
+while(pasword !=="1234"){
+    pasword=prompt("Enter the password");
+}
+console.log("Access granted");
+let f=1;
+// while(f<=5){
+//     console.log(f);
+//     f++
+// }
+
+// let z=10;
+// while(z<=10){
+//     console.log(z)
+//     z--
+// }
+// let count=10;
+// while (count>=1) {
+//     console.log(count);
+//     count--
+// }
+let count=5
+do{
+    console.log("count")
+}while(count>=1)
